bd6420da98cdaaaa81198754bba4e2d55b2848d4
M. Shahrim <shahrim.private@gmail.com>
2016-07-05 03:07:28 +0800

    Edited Scrola.java file for independent directory

--- Scrola.java
+++ Scrola.java
@@ -119,7 +119,7 @@ public class Scrola {
     htmlWriter.close();
 
     // Launch the browser.
-    String[] process = {browserPath,"%U", "C:/Projects/Project Scrola/scrola/templates/default/scrolaviewer.html"};
+    String[] process = {browserPath, "%U", System.getProperty("user.dir") + "/templates/default/scrolaviewer.html"};
     ProcessBuilder browserLauncher = new ProcessBuilder(process);
     Process scrolaViewerBrowser = browserLauncher.start();
   }
